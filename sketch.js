a2fde3db672241456c02be46eabd7b051cc380a2
yule026-tech <yule026@gmail.com>
2025-12-15 22:57:12 +0900

    음량 기반 원형 비주얼라이저 추가

--- sketch.js
+++ sketch.js
@@ -1,22 +1,32 @@
 let song;
-let amp;
+let amp; 
 
 function preload() {
-  song = loadSound('시작의 아이.mp3');
+  song = loadSound('청춘만화.mp3');
 }
 
 function setup() {
   createCanvas(400, 400);
-  amp = new p5.Amplitude();
+  amp = new p5.Amplitude(); // Amplitude 생성
 }
 
 function draw() {
   background(220);
 
+  // 현재 음량 값
   let level = amp.getLevel();
 
+  // 음량을 원 크기로 매핑
+  let size = map(level, 0, 0.3, 50, 300);
+
+  fill(150);
+  noStroke();
+  ellipse(width / 2, height / 2, size);
+
+  // 안내 텍스트
+  fill(0);
   textAlign(CENTER, CENTER);
-  text('음량: ' + level.toFixed(3), width / 2, height / 2);
+  text('클릭하면 음악 재생/정지', width / 2, height - 30);
 }
 
 function mousePressed() {
