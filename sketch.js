c47850431b201dfcc6725fd113f52d9cad1de881
yule026-tech <yule026@gmail.com>
2025-12-15 21:30:47 +0900

    p5.Amplitude를 이용한 음량 분석 기능 추가
    Updated the song file and added amplitude visualization.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
 let song;
+let amp;
 
 function preload() {
-  song = loadSound('청춘만화.mp3');
+  song = loadSound('시작의 아이.mp3');
 }
 
 function setup() {
   createCanvas(400, 400);
+  amp = new p5.Amplitude();
 }
 
 function draw() {
   background(220);
+
+  let level = amp.getLevel();
+
   textAlign(CENTER, CENTER);
-  text('클릭하면 음악 재생/정지', width / 2, height / 2);
+  text('음량: ' + level.toFixed(3), width / 2, height / 2);
 }
 
 function mousePressed() {
