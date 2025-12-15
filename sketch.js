bde15ef8cdcb5d75aacb0fc49e115e39b5cfb9f7
yule026-tech <yule026@gmail.com>
2025-12-15 20:47:29 +0900

    음악 파일 로드 및 재생 기능 구현

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+let song;
+
+function preload() {
+  song = loadSound('청춘만화.mp3');
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  textAlign(CENTER, CENTER);
+  text('클릭하면 음악 재생/정지', width / 2, height / 2);
 }
-//엥
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.loop();
+  }
+}
